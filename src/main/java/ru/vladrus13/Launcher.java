4cb4ec2b619cede1e23bf6ff31f8d54abdbac003
vladrus13 <vladrus13rus@yandex.ru>
2020-07-14 15:43:45 +0300

    Add useless while true for working more than 90 sec

--- src/main/java/ru/vladrus13/Launcher.java
+++ src/main/java/ru/vladrus13/Launcher.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ru.vladrus13.bot.ParserBot;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Launcher {
     public static void main(String[] args) {
         ApiContextInitializer.init();
@@ -13,6 +17,14 @@ public class Launcher {
             botsApi.registerBot(new ParserBot());
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
+        }
+        // so useless!!! heroku pls do something with ports. I don't need it!
+        try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(System.getenv("PORT")))) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
